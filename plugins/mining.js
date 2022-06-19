12eef7398bbfa5ed2e774a57fe87419de0f13f93
reyzen321 <107551396+reyzen321@users.noreply.github.com>
2022-06-19 21:21:44 +0700

    Update mining.js

--- plugins/mining.js
+++ plugins/mining.js
@@ -1,59 +1,41 @@
-let { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+    let __timers = (new Date - user.lastmining)
+    let _timers = (300000 - __timers)
+    let timers = clockString(_timers) 
+    let botol = global.botwm
+    let minim = `${Math.floor(Math.random() * 3)}`.trim() 
+    let minam = `${Math.floor(Math.random() * 5)}`.trim() 
+    let minkm = `${Math.floor(Math.random() * 10)}`.trim() 
 
-const miningxp = 5000
-const miningmoney = 5000
-const mininglimit = 10
-const timeout = 3600000
+   if ( user.pickaxe > 0 ) {
+    if (new Date - user.lastmining > 3000000) {
 
-let handler = async (m, { conn, usedPrefix, text }) => {
-	    let time = global.db.data.users[m.sender].lastmining + 3600000
-  if (new Date - global.db.data.users[m.sender].lastmining< 3600000) throw `Anda sudah mengambil hadiah\ntunggu selama ${msToTime(time - new Date())} lagi`
-	//let xpee = `${Math.floor(Math.random(global.db.data.users[m.sender].exp += miningxp) * 5000)}`.trim()
-	// let moneyy = `${Math.floor(Math.random(global.db.data.users[m.sender].money += miningmoney) * 5000)}`.trim()
-	//let limitt = `${Math.floor(Math.random(global.db.data.users[m.sender].limit += mininglimit) * 15)}`.trim()
-	let limitt = `${Math.floor(Math.random() * 15)}`.trim()
-	let xpee = `${Math.floor(Math.random() * 5000)}`.trim()
-	let moneyy = `${Math.floor(Math.random() * 5000)}`.trim()
-	global.db.data.users[m.sender].limit += limitt * 1
-	global.db.data.users[m.sender].exp += xpee * 1
-	global.db.data.users[m.sender].money += moneyy * 1
-	global.db.data.users[m.sender].exp -= 500
-	global.db.data.users[m.sender].money -= 500
-	global.db.data.users[m.sender].lastmining = new Date * 1
-  m.reply(`Selamat kamu mendapatkan : \n+${xpee} XP\n+${moneyy} Money\n+${limitt} Limit\n\nDipotong pajak :\n-500 XP\n-500 Money`)
-  setTimeout(() => {
-					conn.reply(m.chat, `Hadiah sudah bisa di dapatkan kembali`, m)
-					}, timeout)
-}
-handler.help = ['hadiah']
-handler.tags = ['rpgabsen']
-handler.command = /^(hadiah)/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+      user.emas += minim * 1
+      user.iron += minam * 1
+      user.string += minkm * 1
+      user.lastmining = new Date * 1
+            
+    m.reply(`Kamu Menambang Di ${pickRandom(['⛰️Lembah', '⛰️Goa mletre', '🏞️Sungai Selandia', '⛰️Goa texas', '...'])}\n*⚒️Hasil Tambang:* 🪙Emas: *${minim}*\n⛓️Besi: *${minam}*\n🕸️String: *${minkm}*`)
+      } else conn.sendBut( m.chat, `Tunggu 🕓${timers} lagi, untuk menambang`, `${botol}`, `⋮☰ Back`, `.menu`, m)
+    } else conn.sendBut( m.chat, `Kamu Tidak Mempunyai *⛏️Pickaxe* untuk menambang\nBuat Lah Pickaxe menggunakan string kayu dan batu!`, `${wm}`, `Craft Pickaxe`, `.craft pickaxe`,m )
+  }
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = false
-handler.exp = 0
-handler.money = 0
+handler.help = ['mining']
+handler.tags = ['rpg']
+handler.command = /^mining/i
 
 module.exports = handler
 
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-    
-  
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " jam " + minutes + " menit " + seconds + " detik"
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
 }
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
+
+let wm = global.wm
