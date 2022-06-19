5f0b9d775b95442aa6860c4a698149c931cfbbb7
reyzen321 <107551396+reyzen321@users.noreply.github.com>
2022-06-19 21:32:58 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,63 +1,89 @@
-let limit = 30
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+const { servers, yta, ytv } = require('../lib/y2mate')
+let fs = require('fs')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not Found'
+  if (!vid) throw 'Konten Tidak ditemukan'
   let isVideo = /2$/.test(command)
   let yt = false
+  let yt2 = false
   let usedServer = servers[0]
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
       usedServer = server
       break
     } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
     }
   }
-  if (yt === false) throw 'All servers can\'t 😕'
+  if (yt === false) throw 'semua server gagal'
+  if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+let anu =  `
+*Judul:* ${title}
+*Ukuran File Audio:* ${filesizeF}
+*Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
+*Link Sumber:* 
+${vid.url}
+`
+     let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(thumb)).buffer()}, { upload: conn.waUploadToServer }) 
+      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+      templateMessage: {
+          hydratedTemplate: {
+            imageMessage: message.imageMessage, 
+            hydratedContentText: anu,
+            hydratedFooterText: wm, 
+            hydratedButtons: [{
+             urlButton: {
+               displayText: 'Join Here',
+               url: gc
+             }
 
-_*Please wait while processing..*_
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Video 360p',
+               id: `.ytmp4 ${vid.url}`,
+             }
+
+            },
+               {
+             quickReplyButton: {
+               displayText: 'Video 720p',
+               id: `.ytv720 ${vid.url}`,
+             }
+
+            },
+               {
+             quickReplyButton: {
+               displayText: 'Audio',
+               id: `.ytmp3 ${vid.url}`,
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
+handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
-handler.premium = false
-handler.group = true
+handler.command = /^(p|play)$/i
 
 handler.exp = 0
-handler.limit = true
 
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
