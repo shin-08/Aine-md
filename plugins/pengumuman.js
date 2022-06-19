d4d3787014a45458d70c4e9b069742efde9986e4
reyzen321 <107551396+reyzen321@users.noreply.github.com>
2022-06-19 21:25:36 +0700

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -1,37 +1,25 @@
-let { generateWAMessageFromContent } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => u.id)
-  let q = m.quoted ? m.quoted : m
-  let c = m.quoted ? m.quoted : m.msg
-  let msg = await conn.cMod(
-    m.chat,
-    await generateWAMessageFromContent(
-      m.chat,
-      {
-        [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-          text: await c || ''
-        },
-        mentions: await users
-      }, {
-        quoted: null,
-        userJid: conn.user.id
-      }
-    ),
-    text || q.text, conn.user.jid,
-    {
-      mentions: await users
-    }
-  )
-  await conn.relayMessage(m.chat, msg.message, {
-    messageId: msg.key.id
-  })
+let handler = async(m, { conn, text, participants }) => {
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
 }
-handler.help = ['hidetag'].map(v => v + ' [teks]')
+     conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, {quoted:fkontak})
+}
+handler.help = ['hidetag <pesan>']
 handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
+handler.command = /^(hidetag)$/i
 
 handler.group = true
 handler.admin = true
-handler.limit = true
 
 module.exports = handler
